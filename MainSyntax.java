9915e94ff18e76b966e57bdba06a2658ae9857fb
ZARGHAM AHMED KHAN <zarghamahmedkhan@gmail.com>
2021-05-24 22:50:43 +0530

    Add files via upload
    PROGRAM TO ACCESS STATIC MEMBER  WITHIN CLASS

---
+++ MainSyntax.java
@@ -0,0 +1,19 @@
+class MainSyntax
+{
+
+	static int a=7;
+	static int b =8;
+	static void display()
+	{
+		int c =a+b;
+		System.out.println("print the above statement"+c);
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println("Start of Java Code");
+		System.out.println(" a is"+a);
+		System.out.println("b is"+b);
+	display();
+	System.out.println(" End of Java Code");
+	}
+}
