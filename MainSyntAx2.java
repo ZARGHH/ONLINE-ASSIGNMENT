d0af2f80bf2383953a6f340f9fe4f371eb9b05a3
ZARGHAM AHMED KHAN <zarghamahmedkhan@gmail.com>
2021-05-24 22:52:08 +0530

    Add files via upload
    PROGRAM TO ACCESS STATIC MEMBER  WITHIN CLASS

---
+++ MainSyntAx2.java
@@ -0,0 +1,17 @@
+class MainSyntAx2
+{
+	 static String username="ZAIMY";
+	 static double cfees= 2345.890;
+	 static void display()
+	{
+		 System.out.println("Displaying the above detail");
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println("Start of Java code");
+		System.out.println("username is"+username);
+		System.out.println("College fees is"+cfees);
+		display();
+		System.out.println("End Of Java Code!");
+	}
+}
