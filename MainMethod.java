7cf8315edf54fa535a7c7ba57604027fce85c858
ZARGHAM AHMED KHAN <zarghamahmedkhan@gmail.com>
2021-05-24 22:56:00 +0530

    Add files via upload
    PROGRAM TO  ACCESS STATIC MEMBER  FROM DIFFERENT CLASS

---
+++ MainMethod.java
@@ -0,0 +1,18 @@
+class Multiply
+{
+	static int a=7;
+	static int b=2;
+	static int prod=a*b;
+}
+
+class MainMethod 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Start Of Java Code!");
+       System.out.println("a is"+Multiply.a);
+	   System.out.println("b is"+Multiply.b);
+	   System.out.println("Product is"+Multiply.prod);
+	   System.out.println("End of Java Code");
+	}
+}
